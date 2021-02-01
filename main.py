9da4e9ed6f3f467893f293c20385d79654bb28a5
Tejas Vyas <tejas1794@users.noreply.github.com>
2021-01-31 21:28:44 -0500

    Step 5: Added simple welcome message print program

---
+++ main.py
@@ -0,0 +1,9 @@
+# AI 2004 - Lab 1 - Tejas Vyas
+
+def print_welcome(project_name):
+    """Print a welcome message for the given project name."""
+    print(f'Hi there, welcome to {project_name}')
+
+
+if __name__ == '__main__':
+    print_welcome('AIDI 2004 - AI in Enterprise Systems')
